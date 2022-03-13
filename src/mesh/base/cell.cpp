cdc3bf570d3bd4d8c21e1f01e620b95618cbc7e7
FredyTP7 <altorpon@upv.es>
2022-03-14 00:52:06 +0100

    Implementation of Volume calculation

--- src/mesh/base/cell.cpp
+++ src/mesh/base/cell.cpp
@@ -16,18 +16,18 @@ Cell::Cell()
     _faces = {};
 }
 
-Cell::Cell(std::array<Node*, 3> _nodes_, std::array<Face*, 3> _faces_) : _nodes(_nodes_), _faces(_faces_)
+Cell::Cell(std::vector<Node*> _nodes_, std::vector<Face*> _faces_) : _nodes(_nodes_), _faces(_faces_)
 {
     this->build();
 }
 
-void Cell::setNodes(std::array<Node*, 3> _nodes_)
+void Cell::setNodes(std::vector<Node*> _nodes_)
 {
     ComplexGeometry::modified();
     _nodes = _nodes_;
 }
 
-void Cell::setFaces(std::array<Face*, 3> _faces_)
+void Cell::setFaces(std::vector<Face*> _faces_)
 {
     ComplexGeometry::modified();
     _faces = _faces_;
@@ -38,12 +38,25 @@ void Cell::_updateCentroid()
     _centroid = (_nodes[0]->pos() + _nodes[1]->pos() + _nodes[2]->pos()) / 3;
 }
 
+void Cell::_updateVolume()
+{
+    _volume = 0.0;
+
+    size_t j = _nodes.size() - 1;
+    for (size_t i = 0; i < _nodes.size(); i++)
+    {
+        _volume += (_nodes.at(j)->pos().x() + _nodes.at(i)->pos().x()) * (_nodes.at(j)->pos().y() - _nodes.at(i)->pos().y());
+        j = i;
+    }
+    _volume= abs(_volume / 2.0);
+
+}
+
 void Cell::build()
 {
     ComplexGeometry::build();
     this->_updateCentroid();
-    //TODO: computeVolume
-    //this->_updateVolume();
+    this->_updateVolume();
 }
 
 Cell::~Cell()
