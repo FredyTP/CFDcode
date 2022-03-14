240e2231fb2c8dd583de3faff6c6f9d22bec5dbe
FredyTP7 <altorpon@upv.es>
2022-03-14 21:08:39 +0100

    Creted MaterialFactory

---
+++ inc/material/material_factory.h
@@ -0,0 +1,55 @@
+/*****************************************************************//**
+ * \file   material_factory.h
+ * \brief  
+ * 
+ * 
+ * \author alfre
+ * \date   March 2022
+ *********************************************************************/
+#pragma once
+
+#include <material/material.h>
+#include <material/properties/properties_base.h>
+#include <memory>
+
+
+namespace material
+{
+class MaterialFactory
+{
+public:
+    MaterialFactory()
+    {
+        this->reset();
+    }
+    void reset()
+    {
+        _material = std::make_unique<Material>();
+    }
+    template <class _DensityModel, class... _Types> void setDensityModel(_Types&&... _Args)
+    {
+        _material->_density = std::make_unique<_DensityModel>(_Args...);
+    }
+    template <class _ViscosityModel, class... _Types> void setViscosityModel(_Types&&... _Args)
+    {
+        _material->_viscosity = std::make_unique<_ViscosityModel>(_Args...);
+    }
+    template <class _ConductivityModel, class... _Types> void setConductivityModel(_Types&&... _Args)
+    {
+        _material->_conductivity = std::make_unique<_ConductivityModel>(_Args...);
+    }
+    std::unique_ptr<Material> extractMaterial()
+    {
+        auto temp = std::move(_material);
+        this->reset();
+        return std::move(temp);
+    };
+
+private:
+    std::unique_ptr<Material> _material;
+};
+
+
+
+
+}
