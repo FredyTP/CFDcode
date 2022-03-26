d149d58e48f8436d52ac53c324143151daf3b0e4
FredyTP7 <altorpon@upv.es>
2022-03-26 21:14:46 +0100

    Modified ComplexGeometry -> BaseGeometry

--- inc/mesh/base/base_geometry.h
+++ inc/mesh/base/base_geometry.h
@@ -11,15 +11,21 @@ namespace mesh
 {
 
 
-class ComplexGeometry
+class BaseGeometry
 {
+    
 public:
     virtual bool isBuild() final { return _build; }
+    void setIndex(size_t _index_) { _index = _index_; }
+    size_t index() const { return _index; }
 protected:
+    BaseGeometry() {}
+    BaseGeometry(size_t _index_) :_index(_index_) {}
     virtual void build() { _build = true; }
     virtual void modified() final { _build = false; }
 private:
     bool _build = false;
+    size_t _index = -1;
 
 };
 
