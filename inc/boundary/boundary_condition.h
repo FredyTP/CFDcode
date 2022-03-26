7a8c24aa9fd9868ac97494e2e403908b24482b68
FredyTP7 <altorpon@upv.es>
2022-03-26 21:11:12 +0100

    Added boundary conditions

--- inc/boundary/boundary_condition.h
+++ inc/boundary/boundary_condition.h
@@ -7,9 +7,16 @@
  * \date   March 2022
  *********************************************************************/
 #pragma once
+
+//STL
+#include <fstream>
+
+//CFD
 #include <mesh/base/face.h>
 #include <field/field.h>
-#
+#include <math/convective/convective_term.h>
+#include <math/var/system_submatrix.h>
+#include <math/diffusive/diffusive_term.h>
 namespace bc
 {
 
@@ -17,32 +24,69 @@ class BoundaryCondition
 {
 public:
     BoundaryCondition() {}
-    virtual std::pair<std::vector<Eigen::Triplet<double>>, Eigen::VectorXd> getBCCoefs(const mesh::Mesh* pmesh,field::Field* field) const
-    {
-        Eigen::VectorXd indep(pmesh->cells()->size());
-        std::vector<Eigen::Triplet<double>> triplets;
-        indep.setZero();
+    virtual void getBCCoefs(math::SystemSubmatrix *submatrix,const field::Fields* field) const
+    {     
         for (auto face : _boundary)
-        {
-            size_t idx = face->getOtherCell(nullptr)->index();
-            auto pair = this->getBoundaryCondition(face->getOtherCell(nullptr), face, field);
-            
-            indep(idx) += pair.second;
-            for (auto triplet : pair.first)
-            {
-                triplets.push_back(triplet);
-            }
-        }
-        return std::pair<std::vector<Eigen::Triplet<double>>, Eigen::VectorXd > (triplets, indep);
+        {          
+             this->getBoundaryCondition(submatrix,face->getOtherCell(nullptr), face, field);            
+        }       
     };
-    virtual std::pair<std::vector<Eigen::Triplet<double>>, double> getBoundaryCondition(const mesh::Cell* cell, const mesh::Face* face, field::Field* field) const = 0;
+    virtual void getBoundaryCondition(math::SystemSubmatrix* submatrix,const mesh::Cell* cell, const  mesh::Face* face,const field::Fields* field) const = 0;
     void addFace(mesh::Face* _face_)
     {
         _boundary.push_back(_face_);
     }
-    
-private:
+    void loadBoundaryCondition(const std::string& path,const mesh::Mesh *mesh_)
+    {
+        std::ifstream file;
+        file.open(path);
+        if (file.is_open())
+        {
+            std::string bcstring;
+            std::getline(file, bcstring);        
+
+            std::vector<size_t> node_ids;
+            std::string node_id_string;
+            std::istringstream stream(bcstring);
+
+
+            while (std::getline(stream, node_id_string, '\t'))
+            {
+                node_ids.push_back(std::stoi(node_id_string)-1);
+            }
+            this->setFacesFromNodes(mesh_, node_ids);
+        }
+    }
+    void setFacesFromNodes(const mesh::Mesh* mesh_, std::vector<size_t>& node_ids_)
+    {
+        for (size_t i = 0; i < node_ids_.size() - 1; i++)
+        {
+            mesh::Face tempface;
+            size_t node1idx = node_ids_[i];
+            size_t node2idx = node_ids_[i + 1];
+            tempface.setNodes(mesh_->nodes()->at(node1idx).get(), mesh_->nodes()->at(node2idx).get());
+            for (size_t faceid = 0; faceid < mesh_->faces()->size(); faceid++)
+            {
+                mesh::Face* addface = mesh_->faces()->at(faceid).get();
+                if (tempface.isEqual(addface))
+                {
+                    _boundary.push_back(addface);
+                }
+            }
+        }
+    }
+    void setConvetiveTerm(math::convective::ConvectiveTerm* _convectiveTerm_)
+    {
+        _convectiveTerm = _convectiveTerm_;
+    }
+    void setDiffusiveTerm(math::diffusive::DiffusiveTerm* _diffusiveTerm_)
+    {
+        _diffusiveTerm = _diffusiveTerm_;
+    }
+protected:
     std::vector<mesh::Face*> _boundary;
+    math::convective::ConvectiveTerm* _convectiveTerm = nullptr;
+    math::diffusive::DiffusiveTerm* _diffusiveTerm = nullptr;
 };
 
 }
--- inc/boundary/constant_temperature.h
+++ inc/boundary/constant_temperature.h
@@ -11,33 +11,51 @@
 #include <mesh/base/cell.h>
 #include <mesh/base/face.h>
 #include <field/field.h>
+#include <math/var/system_submatrix.h>
 
 namespace bc
 {
     class ConstantTemperature : public BoundaryCondition
     {
     public:
-        ConstantTemperature(double _temperature_) :_temperature(_temperature_) {};
+        ConstantTemperature(double _temperature_) :_temperature(_temperature_) {
+            _convectiveTerm = nullptr;
+            _diffusiveTerm = nullptr;
+        };
         
-
-        virtual std::pair<std::vector<Eigen::Triplet<double>>, double> getBoundaryCondition(const mesh::Cell* cell, const  mesh::Face* face, field::Field* field) const
+        virtual void getBoundaryCondition(math::SystemSubmatrix* submatrix,const mesh::Cell* cell, const  mesh::Face* face, const field::Fields* field) const
         {
-            std::vector<Eigen::Triplet<double>> coefs;
-            //THIS IS NOT GOOD --- CORRECT IN THE FUTURE -- Only works for constant values
-            //field::StateVector state;
-            double k = 1000;//cell->material()->conductivity(state);
-            double flux = k * face->getUnitFlux(cell);
-            double rho = 1.225;
-            double cv = 1048;
-            coefs.push_back(Eigen::Triplet<double>(cell->index(), cell->index(), flux));
-            double difusiveSource = _temperature * flux;
-            double convectiveSource = _temperature * cv * rho * face->area() * field->velocityField()->at(face->index()).dot(face->getNormal(cell));
-            double column = (difusiveSource - convectiveSource);
-            return std::pair<std::vector<Eigen::Triplet<double>>, double>(coefs, column);
+            
+            if (_convectiveTerm != nullptr)
+            {
+                _convectiveTerm->calculateFaceConvectionBoundary(submatrix,cell, face, _temperature, field);;
+            }
+            if (_diffusiveTerm != nullptr)
+            {
+                _diffusiveTerm->calculateFaceDiffusionBoundary(submatrix,cell, face, _temperature, field);
+                
+            }
+            
         }
-
 
     private:
         double _temperature;
     };
 }
+
+//OLD ONE
+/*virtual std::pair<std::vector<Eigen::Triplet<double>>, double> getBoundaryCondition(const mesh::Cell* cell, const  mesh::Face* face, field::Field* field) const
+{
+    std::vector<Eigen::Triplet<double>> coefs;
+    //THIS IS NOT GOOD --- CORRECT IN THE FUTURE -- Only works for constant values
+    //field::StateVector state;
+    double k = 1000;//cell->material()->conductivity(state);
+    double flux = k * face->getUnitFlux(cell);
+    double rho = 1.225;
+    double cv = 1048;
+    coefs.push_back(Eigen::Triplet<double>(cell->index(), cell->index(), flux));
+    double difusiveSource = _temperature * flux;
+    double convectiveSource = _temperature * cv * rho * face->area() * field->velocityField()->at(face->index()).dot(face->getNormal(cell));
+    double column = (difusiveSource - convectiveSource);
+    return std::pair<std::vector<Eigen::Triplet<double>>, double>(coefs, column);
+}*/
