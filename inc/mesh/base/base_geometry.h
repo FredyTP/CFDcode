38a4f83f0ff83e01248c738980ad6c88c335449c
FredyTP7 <altorpon@upv.es>
2022-04-20 21:28:26 +0200

    Made position method public

--- inc/mesh/base/base_geometry.h
+++ inc/mesh/base/base_geometry.h
@@ -18,13 +18,15 @@ public:
     virtual bool isBuild() final { return _build; }
     void setIndex(size_t _index_) { _index = _index_; }
     size_t index() const { return _index; }
+    const vector2d& position() const { return _pos; }
 protected:
     BaseGeometry() {}
     BaseGeometry(size_t _index_) :_index(_index_) {}
     virtual void build() { _build = true; }
     virtual void modified() final { _build = false; }
-    const vector2d& position() const { return _pos; }
 
+    //Position or centroid coordinates
+    // ( x[m] , y[m] )
     vector2d _pos;
 private:
     bool _build = false;
