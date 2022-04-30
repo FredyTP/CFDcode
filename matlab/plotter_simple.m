26c7339a8124851c153ab9712a06fdb2a206cd4c
FredyTP7 <altorpon@upv.es>
2022-04-30 02:22:32 +0200

    update_plotter

--- matlab/plotter_simple.m
+++ matlab/plotter_simple.m
@@ -1,21 +1,30 @@
-data = csvread("../static_solutionOC.txt",1);
+clear all
+close all
 
+data = csvread("static_solution.txt",1);
 
-x=data(:,1);
-y=data(:,2);
-Tc=data(:,3);
-figure
-scatter3(x,y,Tc)
-hold on
-
-data = csvread("../static_solutionCDs.txt",1);
+%%
 x=data(:,1);
 y=data(:,2);
 T=data(:,3);
+xv=data(:,4:7)';%x coordinates of vertices
+yv=data(:,8:11)';%y coordinates of vertices
+zv=[T';T';T'];
 
-scatter3(x,y,T)
+color = colormap;
+maxt = max(T);
+mint = min(T);
+diff = max(maxt-mint,1);
+colors=round((T-mint)/diff*255);
 
-legend("ORTH-CORR","CDs")
+minx = min(min(xv));
+maxx = max(max(xv));
+miny=min(min(yv));
+maxy=max(max(yv));
+width = maxx-minx;
+height = maxy-miny;
+pbaspect([width/height 1 1])
+patch(xv,yv,colors,'EdgeColor','none');
 
-figure
-scatter3(x,y,Tc-T)
+
+
