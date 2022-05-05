f290aa7c72ae988c2b91aa178b6e996905af206e
FredyTP7 <altorpon@upv.es>
2022-05-05 13:42:30 +0200

    created sampled line for post-process

--- inc/post/sampled_line.h
+++ inc/post/sampled_line.h
@@ -12,48 +12,16 @@
 #include <math/interpolation/gradient_interpolation.h>
 namespace post
 {
-    class SampledLine
+    class PostProcessLine
     {
     public:
-        SampledLine(vector2d origin, vector2d end, uint32_t samples)
-            : _origin(origin), _end(end), _samples(samples)
+        PostProcessLine(vector2d origin, vector2d end)
+            : _origin(origin), _end(end)
         {
             
         }
 
-        void sampleSolution(const sys::Problem* problem, Eigen::VectorXd& solution, bool gradientCorrection = false)
-        {
-            vector2d dr = (_end - _origin) / (_samples - 1);
-            for (int i = 0; i < _samples; i++)
-            {
-                vector2d deltaPos = i * dr;
-                vector2d pos = _origin + deltaPos;
-                const mesh::Cell* cell = SampledLine::findInsideCell(pos, problem);
-                if (cell != nullptr)
-                {
-                    _distance.push_back(deltaPos.norm());
-
-                    //USE GRADIENT CORRECTION
-                    double temp = solution(cell->index());
-                    if (gradientCorrection)
-                    {
-                        vector2d vect = pos - cell->position();
-                        std::vector<math::CellValue<vector2d>> gradient;
-                        std::vector<math::CellValue<double>> cellvalues;
-                        math::GradientInterpolation::GreenGaussGradient(cell, gradient);
-                        math::GradientInterpolation::DotProduct(cellvalues, gradient, vect);
-                        double deltaT = 0;
-                        for (auto& val : cellvalues)
-                        {
-                            deltaT += val.coef * solution(val.cell->index());
-                        }
-                        temp += deltaT;
-                    }
-
-                    _value.push_back(temp);
-                }
-            }
-        }
+        virtual void sampleSolution(const sys::Problem* problem, Eigen::VectorXd& solution, bool gradientCorrection = false) = 0;     
 
         void saveResults(const std::string& filename)
         {
@@ -72,11 +40,25 @@ namespace post
             }
 
         }
-        const static mesh::Cell* findInsideCell(const vector2d& point, const sys::Problem* problem)
+        static double GradientCorrection(const vector2d& pos,const mesh::Cell* cell, const Eigen::VectorXd& solution)
+        {
+            vector2d vect = pos - cell->position();
+            std::vector<math::CellValue<vector2d>> gradient;
+            std::vector<math::CellValue<double>> cellvalues;
+            math::GradientInterpolation::GreenGaussGradient(cell, gradient);
+            math::GradientInterpolation::DotProduct(cellvalues, gradient, vect);
+            double deltaT = 0;
+            for (auto& val : cellvalues)
+            {
+                deltaT += val.coef * solution(val.cell->index());
+            }
+            return deltaT;
+        }
+        const static mesh::Cell* FindInsideCell(const vector2d& point, const sys::Problem* problem)
         {
             for(auto& cell: *problem->mesh()->cells())
             {
-                if (SampledLine::isPointInsideCell(point, cell.get()))
+                if (PostProcessLine::IsPointInsideCell(point, cell.get()))
                 {
                     return cell.get();
                 }
@@ -85,7 +67,7 @@ namespace post
         }
 
 
-        static bool isPointInsideCell(const vector2d& point, const mesh::Cell* cell)
+        static bool IsPointInsideCell(const vector2d& point, const mesh::Cell* cell)
         {
             bool isInside = true;
             //Check if cell is a boundary cell
@@ -104,12 +86,161 @@ namespace post
             }
             return isInside;
         }
-    private:
+    protected:
         vector2d _origin;
         vector2d _end;
-        double _samples;
 
         std::vector<double> _distance;
         std::vector<double> _value;
     };
+
+    class SampledLine : public PostProcessLine
+    {
+    public:
+        SampledLine(vector2d origin, vector2d end, uint32_t samples)
+            : PostProcessLine(origin,end), _samples(samples)
+        {
+
+        }
+
+        void sampleSolution(const sys::Problem* problem, Eigen::VectorXd& solution, bool gradientCorrection = false)
+        {
+            vector2d dr = (_end - _origin) / (_samples - 1);
+            for (int i = 0; i < _samples; i++)
+            {
+                vector2d deltaPos = i * dr;
+                vector2d pos = _origin + deltaPos;
+                const mesh::Cell* cell = PostProcessLine::FindInsideCell(pos, problem);
+                if (cell != nullptr)
+                {
+                    _distance.push_back(deltaPos.norm());
+
+                    //USE GRADIENT CORRECTION
+                    double temp = solution(cell->index());
+                    if (gradientCorrection)
+                    {
+                        temp += PostProcessLine::GradientCorrection(pos, cell, solution);
+                    }
+
+                    _value.push_back(temp);
+                }
+            }
+        }
+
+    private:
+        double _samples;
+    };
+
+    class CellSampledLine : public PostProcessLine
+    {
+    public:
+        CellSampledLine(vector2d origin, vector2d end, uint32_t deltar)
+            : PostProcessLine(origin, end), _deltar(deltar)
+        {
+
+        }
+
+        void sampleSolution(const sys::Problem* problem, Eigen::VectorXd& solution, bool gradientCorrection = false)
+        {
+            vector2d lineDirection = (_end - _origin).normalized();
+            vector2d dr = lineDirection * _deltar;
+            const mesh::Cell* lastcell = nullptr;
+            int tries = 0;
+            while (tries < 1000)
+            {
+                vector2d deltaPos = tries * dr;
+                vector2d pos = _origin + deltaPos;
+                const mesh::Cell* cell = PostProcessLine::FindInsideCell(pos, problem);
+                if (cell != nullptr)
+                {
+                    double maxdotprod = -10000;
+                    vector2d cellpos = cell->position();
+                    mesh::Cell* max_cell = nullptr;
+                    mesh::Face* max_face = nullptr;
+                    for (auto face : cell->faces())
+                    {
+
+                        vector2d dir = (face->getOtherCell(cell)->position() - cellpos).normalized();
+                        double dotprod = -lineDirection.dot(dir);
+                        if (dotprod > maxdotprod)
+                        {
+                            maxdotprod = dotprod;
+                            max_cell = face->getOtherCell(cell);
+                            max_face = face;
+                        }
+                      
+
+                    }
+
+                    if (max_face!=nullptr && max_cell!=nullptr && max_face->isBoundary())
+                    {
+                        //IF BOUNDARY FACE DETECTED
+                        _distance.push_back(0);
+
+                        //USE GRADIENT CORRECTION
+                        double temp = solution(max_cell->index());
+                        _value.push_back(temp);
+
+                        vector2d cell2cell = (cell->position() - max_cell->position());
+                        double deltar = cell2cell.dot(lineDirection);
+                        _distance.push_back(deltar + _distance.back());
+
+                        temp = solution(cell->index());
+                        _value.push_back(temp);
+                    }
+                    else
+                    {
+                        _distance.push_back(0);
+
+                        double temp = solution(cell->index());
+                        _value.push_back(temp);
+                    }
+
+                    
+                    while (1)
+                    {
+                        double maxdotprod = -10000;
+                        vector2d cellpos = cell->position();
+                        mesh::Cell* max_cell = nullptr;
+                        for (auto face : cell->faces())
+                        {
+                            vector2d dir = (face->getOtherCell(cell)->position() - cellpos).normalized();
+                            double dotprod = lineDirection.dot(dir);
+                            if (dotprod > maxdotprod)
+                            {
+                                maxdotprod = dotprod;
+                                max_cell = face->getOtherCell(cell);
+                            }
+                        }
+                        if (maxdotprod > 0)
+                        {
+                            vector2d cell2cell = (max_cell->position() - cellpos);
+                            double deltar = cell2cell.dot(lineDirection);
+                            _distance.push_back(deltar + _distance.back());
+                            double temp = solution(max_cell->index());
+
+                            _value.push_back(temp);
+                            cell = max_cell;
+                        }
+                        else
+                        {
+                            tries = 1000;
+                            break;
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    tries++;
+                }
+            }
+             
+        }
+
+    private:
+        double _deltar;
+    };
+
 }
